c98e588ff1b8b77806cc9272dd0b1098c639f8bf
Marvin Rabe <marvin.rabe@gmail.com>
2019-01-08 13:43:06 +0100

    Moved init hook.

--- src/init.ts
+++ src/init.ts
@@ -1,7 +1,7 @@
 import { Hook } from '@oclif/config'
-import InversifyPlugin from '../../plugin'
+import InversifyPlugin from './plugin'
 
-const hook: Hook<'init'> = async function (opts) {
+const hook: Hook<'init'> = async function () {
 
   this.config.plugins.push(new InversifyPlugin(this.config))
 
