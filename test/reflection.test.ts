0f46e14aace62ec156dbe06defee9bdc5446a345
Marvin Rabe <marvin.rabe@gmail.com>
2019-01-08 13:42:22 +0100

    Added additional test.

--- test/reflection.test.ts
+++ test/reflection.test.ts
@@ -1,5 +1,5 @@
 import * as expect from 'expect'
-import { getCommandMetadata } from '../src/reflection'
+import { addCommandMetadata, getCommandMetadata } from '../src/reflection'
 
 describe('Reflection', () => {
   it('returns metadata', () => {
@@ -8,4 +8,22 @@ describe('Reflection', () => {
     expect(commands).toContain('simple')
     expect(commands).toContain('complex:foo')
   })
+
+  it('adds metadata', () => {
+    const length = getCommandMetadata().length
+
+    addCommandMetadata({
+      command: 'my-funky-command',
+      target: null
+    })
+
+    const metadata = getCommandMetadata()
+
+    expect(metadata.length).toBe(length + 1)
+
+    const commands = metadata.map(x => x.command)
+
+    expect(commands).toContain('my-funky-command')
+  })
+
 })
